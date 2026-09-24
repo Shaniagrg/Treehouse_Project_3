dcb14d04d080bb46a9930ec6196ffd595e769d04
Shaniagrg <sglearner2022@gmail.com>
2026-09-24 15:43:29 -0700

    use focus() method on id=name so that when page loads it  will have focus on name input field by default

--- interactive-form-v3/js/script.js
+++ interactive-form-v3/js/script.js
@@ -1,1 +1,3 @@
-console.log('Test');
+//Use the focus() method so when the page first loads the first text field will have the focus state by default to prompt the user
+const nameInput = document.getElementById('name');
+nameInput.focus()
