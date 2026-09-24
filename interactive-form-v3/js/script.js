b68fa34bcdefb4f074b40f1b83fc20649f7f64b5
Shaniagrg <sglearner2022@gmail.com>
2026-09-24 16:27:15 -0700

    use eventlistener when a change is detected, display/hide the "text field" for "other" based on the selection in the drop-down menu.

--- interactive-form-v3/js/script.js
+++ interactive-form-v3/js/script.js
@@ -1,3 +1,23 @@
 //Use the focus() method so when the page first loads the first text field will have the focus state by default to prompt the user
 const nameInput = document.getElementById('name');
-nameInput.focus()
+nameInput.focus();
+
+//#other-job-role hidden line 50
+//Store the job element inside a variable
+const jobRole = document.getElementById('title');
+const otherJobRole = document.getElementById('other-job-role');
+
+//Hide #other-job-role so it is not displayed when the form first loads
+otherJobRole.style.display = 'none';
+
+jobRole.addEventListener('change', (e) => {
+    //Display the #other-job-role input field when user clicks "other"
+    if (e.target.value === 'other'){
+        otherJobRole.style.display = 'block';
+    }else{
+        //Hidden again when user doesn't click on "other" 
+        otherJobRole.style.display = 'none';
+        //clears the text written inside the description and Your will have new inputfield that is blank again
+        otherJobRole.value = "";
+    }
+});
